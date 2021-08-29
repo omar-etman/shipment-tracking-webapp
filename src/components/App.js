3faa3ae81608560f08abaf2a774b400730f319db
omar-etman <omartareketman@gmail.com>
2021-08-29 23:31:38 +0200

    Setup the App's general preliminary structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import '../styles/App.css';
 import HeaderNav from './HeaderNav';
+import ShipmentTracker from './ShipmentTracker';
+import DetailsTable from './DetailsTable';
+import IssueReport from './IssueReport';
 
 function App() {
   return (
     <div className="App">
       <HeaderNav/>
+      <div className ="App__body">
+        <ShipmentTracker/>
+        <div className ="body__details">
+          <DetailsTable/>
+          <IssueReport/>
+        </div>
+
+      </div>
     </div>
   );
 }
---
+++ src/components/DetailsTable.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DetailsTable() {
+    return (
+        <div>
+            table
+        </div>
+    )
+}
+
+export default DetailsTable
